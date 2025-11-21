e4bd1391699f1c24d10c4cda878c42ce6ab4a7ce
TobiasCaste <castellarintobias@gmail.com>
2025-11-21 00:02:31 -0300

    TO DO

--- castellarin-autorepuestos/src/main/java/com/castellarin/autorepuestos/CastellarinAutorepuestosApplication.java
+++ castellarin-autorepuestos/src/main/java/com/castellarin/autorepuestos/CastellarinAutorepuestosApplication.java
@@ -8,6 +8,12 @@ import java.util.TimeZone;
 @SpringBootApplication
 public class CastellarinAutorepuestosApplication {
 
+    /*TODO
+    *  IMPLEMENTAR LOGICA DE COMPRA (MERCADO PAGO)
+    * IMPLEMENTAR LOGICA DE CALCULO DE ENVIO
+    * IMPLEMENTAR LOGICA DE INICIO DE SESION
+    * */
+
 	public static void main(String[] args) {
 		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 		SpringApplication.run(CastellarinAutorepuestosApplication.class, args);
