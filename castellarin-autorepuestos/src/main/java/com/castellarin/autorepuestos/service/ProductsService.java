bfeb879c41fc9835475e7aaca9ff6aef37e04ecc
Tobias <138527873+Tobias027@users.noreply.github.com>
2026-01-29 17:42:38 -0300

    Implementation of cloudinary to manage images of products

--- castellarin-autorepuestos/src/main/java/com/castellarin/autorepuestos/service/ProductsService.java
+++ castellarin-autorepuestos/src/main/java/com/castellarin/autorepuestos/service/ProductsService.java
@@ -19,6 +19,7 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
@@ -101,7 +102,7 @@ public class ProductsService {
         Product product = new Product();
         ProductBrand brand = productBrandRepository.findProductBrandByBrand(createProductDto.getBrand());
         Category category = categoryRepository.findByCategory(createProductDto.getCategory());
-        String imagePath = imageService.uploadProductsImage(createProductDto.getBase64Image(),createProductDto.getName());
+        Map uploadResult = imageService.uploadProductsImage(createProductDto.getBase64Image(),createProductDto.getPartNumber());
 
         product.setName(createProductDto.getName());
         product.setBrand(brand);
@@ -112,7 +113,7 @@ public class ProductsService {
         product.setPrice(createProductDto.getPrice());
         product.setOfferPrice(createProductDto.getOfferPrice());
         product.setStock(createProductDto.getStock());
-        product.setImagePath(imagePath);
+        product.setImagePath(uploadResult.get("url").toString());
         product.setIsActive(createProductDto.getIsActive());
         product.setCostPrice(createProductDto.getCostPrice());
         product.setNotes(createProductDto.getNotes());
