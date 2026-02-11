073196f5d712bb6796e63825c8d6083f37c2e342
Tobias <138527873+Tobias027@users.noreply.github.com>
2026-02-11 16:15:06 -0300

    check notifications

--- castellarin-autorepuestos/src/main/java/com/castellarin/autorepuestos/controller/PaymentController.java
+++ castellarin-autorepuestos/src/main/java/com/castellarin/autorepuestos/controller/PaymentController.java
@@ -3,17 +3,14 @@ package com.castellarin.autorepuestos.controller;
 import com.castellarin.autorepuestos.domain.dto.OrderDto;
 import com.castellarin.autorepuestos.domain.dto.PreferenceDto;
 import com.castellarin.autorepuestos.domain.entity.*;
-import com.castellarin.autorepuestos.security.SignatureVerifier;
 import com.castellarin.autorepuestos.service.MercadoPagoService;
-import com.castellarin.autorepuestos.service.OrdersService;
+import com.castellarin.autorepuestos.service.OrderOrchestratorService;
+import com.castellarin.autorepuestos.service.PreferenceService;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.mercadopago.client.merchantorder.MerchantOrderClient;
-import com.mercadopago.exceptions.MPApiException;
-import com.mercadopago.exceptions.MPException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -21,13 +18,15 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Map;
 
 @RequestMapping("/payments")
 @RestController
 @RequiredArgsConstructor
 public class PaymentController {
 
-    private final OrdersService ordersService;
+    private final OrderOrchestratorService orderOrchestratorService;
+    private final PreferenceService preferenceService;
     private final MercadoPagoService mercadoPagoService;
     @Value("${mp.test.webhook-secret-key}")
     private String webhookSecret;
@@ -36,9 +35,9 @@ public class PaymentController {
     private String accessToken;
 
     @PostMapping("/create-payment")
-    public ResponseEntity<PreferenceDto> createOrder(@AuthenticationPrincipal User user, @RequestBody OrderDto orderDto){
-        Order order = ordersService.createPendingOrder(user,orderDto);
-        PreferenceDto preferenceDto = mercadoPagoService.createPreference(order);
+    public ResponseEntity<PreferenceDto> createOrder(@RequestBody OrderDto orderDto){
+        Preference preference = preferenceService.createPrefence(orderDto);
+        PreferenceDto preferenceDto = mercadoPagoService.createPreference(preference);
         return ResponseEntity.ok(preferenceDto);
     }
 
@@ -49,10 +48,14 @@ public class PaymentController {
             @RequestHeader("x-request-id") String requestId,
             @RequestBody JsonNode payload) {
 
+        System.out.println(dataId);
+        System.out.println(signature);
+        System.out.println(requestId);
 
         String type = payload.path("type").asText();
         System.out.println(payload);
         switch (type){
+            //EN ESTE CASO SE CREA DE 0
             case "payment":
                 /*String[] parts = signature.split(",");
                 String ts = parts[0].split("=")[1];
@@ -70,10 +73,23 @@ public class PaymentController {
 
                         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-                        System.out.println(response.statusCode());
-                        System.out.println(response.body());
-                        System.out.println(response.headers());
+                        ObjectMapper mapper = new ObjectMapper();
+                        Map<String,Object> responseBody = mapper.readValue(response.body(), Map.class);
 
+
+                        Boolean order = orderOrchestratorService.createOrder(responseBody);
+                        // TODO VER SI ANTES DE CREAR LA PREFERENCIA CHEQUEA STOCK
+                        // Order order = ordersService
+                        // Shipping Address
+                        // Billing Address
+                        //Payment
+                        //Payer Details
+                        //IF CARD PaymentCard
+
+                        //TODO BAJAR STOCK
+                        //TODO ENVIAR UN EMAIL
+                        // ENVIAR MAIL A NOSOTROS POR NUEVA VENTA??
+                        //TODO LOGICA DE CORREO
                     } catch (IOException e) {
                         throw new RuntimeException(e);
                     } catch (InterruptedException e) {
