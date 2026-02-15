ac453fff04c648dfd7398975186d9d72d943dcf7
Tobias <138527873+Tobias027@users.noreply.github.com>
2026-02-14 23:46:41 -0300

    logic after payment implemented

--- castellarin-autorepuestos/src/main/java/com/castellarin/autorepuestos/domain/entity/PaymentCardId.java
+++ castellarin-autorepuestos/src/main/java/com/castellarin/autorepuestos/domain/entity/PaymentCardId.java
@@ -1,10 +1,18 @@
 package com.castellarin.autorepuestos.domain.entity;
 
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Embeddable
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@EqualsAndHashCode
 public class PaymentCardId implements Serializable {
     private Long paymentId;
     private String CardId;
